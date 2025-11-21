1ad755e859da73c81517852de6ac42ca076246e5
Nando <50nando05@gmail.com>
2025-11-21 17:56:03 +0100

    modelo

---
+++ lib/model/producto.dart
@@ -0,0 +1,14 @@
+class Producto {
+  String? nombre;
+  double? precio;
+  Producto(this.nombre, this.precio);
+
+  @override
+  String toString() {
+    return 'Producto{nombre: $nombre, precio: $precio}';
+  }
+}
+// "La interfaz del sabor"
+// "Pixel y pinta"
+// "Bar 404"
+// "UI/UX Bar"
